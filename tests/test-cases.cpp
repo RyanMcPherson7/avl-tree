45050e1d55ec66a01ea2a72e2bc606ca9ed9bb1e
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2021-11-29 23:33:34 -0500

    added many more test cases

--- tests/test-cases.cpp
+++ tests/test-cases.cpp
@@ -3,12 +3,113 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-AvlTree tree;
+TEST_CASE( "Test left rotation", "[Rotation]" ) {
 
-TEST_CASE( "Testing Balancing", "[level-count]" ) {
-
-    tree.insert("ryuan", "12341234");
+    AvlTree tree;
+    tree.insert("Ryan", "11111111");
+    tree.insert("Joe", "22222222");
+    tree.insert("Sandy", "33333333");
     
-
-    REQUIRE(tree.printLevelCount() == 1);
+    REQUIRE(tree.printLevelCount() == 2);
 }
+
+TEST_CASE( "Test right rotation", "[Rotation]" ) {
+
+    AvlTree tree;
+    tree.insert("Sandy", "33333333");
+    tree.insert("Joe", "22222222");
+    tree.insert("Ryan", "11111111");
+
+    REQUIRE(tree.printLevelCount() == 2);
+}
+
+TEST_CASE( "Test right left rotation", "[Rotation]" ) {
+
+    AvlTree tree;
+    tree.insert("Ryan", "11111111");
+    tree.insert("Sandy", "33333333");
+    tree.insert("Joe", "22222222");
+    
+    REQUIRE(tree.printLevelCount() == 2);
+}
+
+TEST_CASE( "Test left right rotation", "[Rotation]" ) {
+
+    AvlTree tree;
+    tree.insert("Sandy", "33333333");
+    tree.insert("Ryan", "11111111");
+    tree.insert("Joe", "22222222");
+    
+    REQUIRE(tree.printLevelCount() == 2);
+}
+
+TEST_CASE( "Test 1,000 insertions", "[Insertion]") {
+
+    AvlTree tree;
+    for (int i = 10000000; i <= 10001000; i++)
+        tree.insert("Ryan", to_string(i));
+
+    REQUIRE(tree.printLevelCount() == 10);
+}
+
+TEST_CASE( "Test 10,000 insertions", "[Insertion]") {
+
+    AvlTree tree;
+    for (int i = 10000000; i <= 10010000; i++)
+        tree.insert("Ryan", to_string(i));
+
+    REQUIRE(tree.printLevelCount() == 14);
+}
+
+TEST_CASE( "Test 100,000 insertions", "[Insertion]") {
+
+    AvlTree tree;
+    for (int i = 10000000; i <= 10100000; i++)
+        tree.insert("Ryan", to_string(i));
+
+    REQUIRE(tree.printLevelCount() == 17);
+}
+
+TEST_CASE( "Test base cases", "[Base Case]") {
+
+    AvlTree tree;
+
+    REQUIRE(tree.printInorder() == "");
+    REQUIRE(tree.printPreorder() == "");
+    REQUIRE(tree.printPostorder() == "");
+    REQUIRE(tree.printLevelCount() == 0);
+}
+
+TEST_CASE( "Test Inorder", "[Traversal]") {
+
+    AvlTree tree;
+    tree.insert("Ryan", "11111111");
+    tree.insert("Mihir", "22222222");
+    tree.insert("Danny", "33333333");
+    tree.insert("Jose", "44444444");
+
+    REQUIRE(tree.printInorder() == "Ryan, Mihir, Danny, Jose");
+}
+
+TEST_CASE( "Test Preorder", "[Traversal]") {
+
+    AvlTree tree;
+    tree.insert("Ryan", "11111111");
+    tree.insert("Mihir", "22222222");
+    tree.insert("Danny", "33333333");
+    tree.insert("Jose", "44444444");
+
+    REQUIRE(tree.printPreorder() == "Mihir, Ryan, Danny, Jose");
+}
+
+TEST_CASE( "Test Postorder", "[Traversal]") {
+
+    AvlTree tree;
+    tree.insert("Ryan", "11111111");
+    tree.insert("Mihir", "22222222");
+    tree.insert("Danny", "33333333");
+    tree.insert("Jose", "44444444");
+
+    REQUIRE(tree.printPostorder() == "Ryan, Jose, Danny, Mihir");
+}
+
